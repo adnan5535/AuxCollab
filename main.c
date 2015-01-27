6e63c0db6b784a67b8d3e048bebbd5cb4ab21c38
adnan5535 <a.dna.n5535@gmail.com>
2015-01-26 18:04:08 -0700

    bleh

--- main.c
+++ main.c
@@ -4,9 +4,14 @@
  *
  * Created on January 26, 2015, 5:49 PM
  */
+#include <p18cxxx.h>
+#include "\Common\J1939.h"
+#include "\Common\ecocar.h"
+#include <delays.h>
+#pragma config OSC = IRCIO67    // Oscillator Selection Bit
+#pragma config BOREN = OFF      // Brown-out Reset disabled in hardware and software
+#pragma config WDT = OFF        // Watchdog Timer disabled (control is placed on the SWDTEN bit)
 
-#include <stdio.h>
-#include <stdlib.h>
 
 /*
  * 
